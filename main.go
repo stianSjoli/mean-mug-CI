f74a10d9a1fec0f4e0ba037300b5ef8012fe79bf
stianSjoli <stianSjoli@users.noreply.github.com>
2023-10-14 01:14:06 +0200

    Update and rename multibuild.go to main.go

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"dagger.io/dagger"
+	"fmt"
+	"os"
+	"sync"
+    "math"
+    "math/rand"
+)
+
+func main() {
+	ctx := context.Background()
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		test(ctx)
+		wg.Done()
+	}()
+    go func() {
+		build(ctx)
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func test(ctx context.Context) {
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	errorCheck(err)
+	root := client.Host().Directory(".")
+	defer client.Close()
+	out, err := client.Container().
+		From("golang:latest").
+		WithDirectory("/", root).
+		WithWorkdir("/").
+		WithExec([]string{"go", "test"}).
+		Stderr(ctx)
+	errorCheck(err)
+	fmt.Println(out)
+}
+
+func build(ctx context.Context) * container {
+    client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	errorCheck(err)
+	root := client.Host().Directory(".")
+	defer client.Close()
+    image, err := root.
+        DockerBuild()
+    errorCheck(err)
+    return image 
+}
+
+func publish(ctx context.Context) {
+    image := build(ctx)
+    ref, err := image.
+        Publish(ctx, fmt.Sprintf("ttl.sh/hello-dagger-%.0f", math.Floor(rand.Float64()*10000000))) //#nosec
+    errorCheck(err)
+    fmt.Printf("Published image to :%s\n", ref)
+}
+
+func errorCheck(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
