e9911b7abe6143a0155797e1e78c20a18f16e55c
stian s <stian.sjoli@knowit.no>
2023-10-17 00:46:09 +0200

    feat(ci): check current dir

--- CI/builder/main.go
+++ CI/builder/main.go
@@ -84,6 +84,9 @@ func writeManifest(manifest Manifest, path string) {
 }	
 
 func main() {
+	dir, dirErr = os.Getwd()
+	fmt.Println(dir)
+	errorCheck(dirErr)
 	ctx := context.Background()
 	imageRef := publish(ctx)
 	manifest := readManifest("./ArgoCD/deployment.yml")
