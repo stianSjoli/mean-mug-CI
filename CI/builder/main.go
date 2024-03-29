a144603e12e51502cb116fab2fc581b794f5e034
stian s <stian.sjoli@knowit.no>
2024-03-29 23:05:31 +0100

    feat(ci): add go threads

--- CI/builder/main.go
+++ CI/builder/main.go
@@ -6,57 +6,60 @@ import (
 	"context"
 	"dagger.io/dagger"
 	"os"
+	"sync"
 	"fmt"
 	"math"
 	"math/rand"
 	"strings"
 	"example.com/git"
     "example.com/manifest"
-	"github.com/magefile/mage/mg"
 )
 
-type App mg.Namespace
+type TargetContext struct {
+	cancel context.CancelFunc
+	ctx context.Context
+}
 
-
-
-func appDirectory() string {
+func appDirectory(cancel context.CancelFunc) string {
 	current, err := os.Getwd()
-	errorCheck(err)
+	errorCheck(err, cancel)
 	return strings.Replace(current, "CI/builder", "App", 1)
 }
 
-func (App) Test(ctx context.Context) {
-	client, errConnect := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+func test(tc TargetContext, wg *sync.WaitGroup) {
+	client, errConnect := dagger.Connect(tc.ctx, dagger.WithLogOutput(os.Stderr))
 	defer client.Close()
-	errorCheck(errConnect)
-	root := client.Host().Directory(appDirectory())
+	errorCheck(errConnect, tc.cancel)
+	root := client.Host().Directory(appDirectory(tc.cancel))
 	_, err := client.Container().
 		From("golang:latest").
 		WithMountedDirectory("/App", root).
 		WithWorkdir("/App").
 		WithExec([]string{"go", "test"}).
-		Stderr(ctx)
-	errorCheck(err)
+		Stderr(tc.ctx)
+	errorCheck(err, tc.cancel)
+	wg.Done()
 }
 
-func (App) Build(ctx context.Context) {
-	client, errConnect := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+func build(tc TargetContext, wg *sync.WaitGroup) {
+	client, errConnect := dagger.Connect(tc.ctx, dagger.WithLogOutput(os.Stderr))
 	defer client.Close()
-	errorCheck(errConnect)
-	root := client.Host().Directory(appDirectory())
+	errorCheck(errConnect, tc.cancel)
+	root := client.Host().Directory(appDirectory(tc.cancel))
 	_, err := client.Container().
 		From("golang:latest").
 		WithMountedDirectory("/App", root).
 		WithWorkdir("/App").
 		WithExec([]string{"go", "build"}).
-		Stderr(ctx)
-	errorCheck(err)
+		Stderr(tc.ctx)
+	errorCheck(err, tc.cancel)
+	wg.Done()
 }
 
-func (App) Deploy(ctx context.Context, token string) {
-	client, errConnect := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
-	errorCheck(errConnect)
-	root := client.Host().Directory(appDirectory())
+func deploy(tc TargetContext, token string) {
+	client, errConnect := dagger.Connect(tc.ctx, dagger.WithLogOutput(os.Stderr))
+	errorCheck(errConnect, tc.cancel)
+	root := client.Host().Directory(appDirectory(tc.cancel))
 	defer client.Close()
 	imageRef := make(chan string)
 	go func() {
@@ -71,8 +74,8 @@ func (App) Deploy(ctx context.Context, token string) {
 			From("alpine").
 			WithFile("/bin/app", builder.File("/src/app")).
 			WithEntrypoint([]string{"/bin/app"})
-		ref, err := prodImage.Publish(ctx, fmt.Sprintf("ttl.sh/app-%.0f", math.Floor(rand.Float64()*10000000))) //default time to live is 1 hour
-		errorCheck(err)
+		ref, err := prodImage.Publish(tc.ctx, fmt.Sprintf("ttl.sh/app-%.0f", math.Floor(rand.Float64()*10000000))) //default time to live is 1 hour
+		errorCheck(err, tc.cancel)
 		imageRef <- ref 
 	}()
 	
@@ -86,11 +89,26 @@ func (App) Deploy(ctx context.Context, token string) {
     git.Commit(manifestPath, repo)
     git.Push(repo, token)
     errRemove := os.RemoveAll(dirPath)
-    errorCheck(errRemove)
+    errorCheck(errRemove, tc.cancel)
 }
 
-func errorCheck(err error) {
+func CI() {
+	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+	wg.Add(2)
+	tc := TargetContext{cancel:cancel, ctx: ctx}
+	go test(tc, &wg)
+	go build(tc, &wg)
+	wg.Wait()
+}
+
+func CD(token string) {
+	ctx, cancel := context.WithCancel(context.Background())
+	tc := TargetContext{cancel:cancel, ctx: ctx}
+	deploy(tc, token)
+}
+func errorCheck(err error, cancel context.CancelFunc) {
 	if err != nil {
-		panic(err)
+		cancel()
 	}
 }
